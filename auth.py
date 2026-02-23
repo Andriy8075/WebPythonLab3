76f210aa1d8c28e63f124352a2e78d9979919472
andriy8075 <andriy8075@gmail.com>
2026-02-23 17:02:43 +0000

    fix encoding on login

--- auth.py
+++ auth.py
@@ -8,11 +8,13 @@ from db import get_db
 from models import User
 
 def get_password_hash(password: str) -> str:
-    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(bytes(plain_password, 'utf-8'), hashed_password)
+    if isinstance(hashed_password, str):
+        hashed_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password)
 
 
 def get_current_user(
